c812478b12de037762482fad412f813934851c5f
Moiz Ahmad <moiznahmad@gmail.com>
2016-05-14 14:41:10 -0700

    Added btnHi -Moiz

--- app/src/main/java/com/ahmilio/turtle/soundcast/MainActivity.java
+++ app/src/main/java/com/ahmilio/turtle/soundcast/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     //whoa
@@ -20,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
 //comment
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();//gf
+            }
+        });
+        Button btnHi = (Button) findViewById(R.id.btnHi);
+        btnHi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
