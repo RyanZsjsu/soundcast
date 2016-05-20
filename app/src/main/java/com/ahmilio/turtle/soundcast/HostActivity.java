8fdaf9dec38be6dee229b48c016dad26cd927a75
Brandon Cecilio <mindlesshavock@gmail.com>
2016-05-20 02:08:07 -0700

    Implemented Song Queue on Host Activity

--- app/src/main/java/com/ahmilio/turtle/soundcast/HostActivity.java
+++ app/src/main/java/com/ahmilio/turtle/soundcast/HostActivity.java
@@ -3,9 +3,7 @@ package com.ahmilio.turtle.soundcast;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
-import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -21,16 +19,16 @@ import android.widget.Switch;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 
 public class HostActivity extends AppCompatActivity {
     private final int REQUEST_CODE = 100;
     private ArrayAdapter<String> queueAdapter;
-    private PlayQueue<String> playQueue;
+    private PlayQueue<Song> playQueue;
     private ListView lvPlayQueue;
     private MediaPlayer mp;
     private File cache;
@@ -46,49 +44,43 @@ public class HostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_host);
 
         lvPlayQueue = (ListView) findViewById(R.id.lvPlayQueue);
-        playQueue = new PlayQueue<>("Hello SoundCast!, Drop It Like It's Hot, Thomas the Tank Engine Theme, imdabes, Never Gonna Give You Up".split(", "));
-        playQueue.enqueue("Ripped Pants");
-        playQueue.enqueue("My House");
-        playQueue.dequeue();
-        playQueue.enqueue("America, Fuck Yeah!");
-        queueAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, playQueue.toArray());
-        lvPlayQueue.setAdapter(queueAdapter);
-        queueAdapter.add("last song");
+//        playQueue = new PlayQueue<>("Hello SoundCast!, Drop It Like It's Hot, Thomas the Tank Engine Theme, imdabes, Never Gonna Give You Up".split(", "));
+//        playQueue.enqueue("Ripped Pants");
+//        playQueue.enqueue("My House");
+//        playQueue.dequeue();
+//        playQueue.enqueue("America, Fuck Yeah!");
+//        queueAdapter.add("last song");
 
-        // simulating file from external storage
-        Log.v("pwd", getApplicationInfo().dataDir);
+        PlayQueue<String> test = new PlayQueue<>("intro cant king superman ridiculous fall bounce affairs want cash fiona".split(" "));
+
         cache = getCacheDir();
         Log.v("created dir", cache.getPath());
+
+        // simulating file from external storage
         File ext = getDir("external", Context.MODE_PRIVATE);
         Log.v("created dir", ext.getPath());
-        File testExtFile = new File(cache+File.separator+"ruby.mp3");
-        Log.v("init testExtFile", testExtFile.getPath());
-        Log.v("ruby deleted", testExtFile.delete() ? "yes" : "no");
-        if (!testExtFile.exists()) try {
-            testExtFile.createNewFile();
-            InputStream is = getResources().openRawResource(
-                    getResources().getIdentifier("ruby", "raw", getPackageName()));
 
-            int size = is.available();
-            Log.v("ruby input size", ""+size);
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
+        playQueue = new PlayQueue<>();
 
-            FileOutputStream fos = new FileOutputStream(testExtFile);
-            fos.write(buffer);
-            fos.close();
-        } catch (IOException e) {
-            Log.e("Write error",e.getMessage());
+        File testExtFile = null;
+        try {
+            while (!test.isEmpty()){
+                String asset = test.dequeue();
+                File cursrc = extFileFromRawAsset(ext,asset,"mp3");
+                Log.v("dir to asset "+asset, cursrc.getPath());
+                Song cursong = new Song(cursrc.getPath(), cache.getPath(), Song.SRC_LOCAL);
+                Log.v("init song "+asset, cursong.getFilename());
+                cursong.cache();
+                Log.v("cached song"+asset, cursong.getCachedCopy().getPath());
+                playQueue.enqueue(cursong);
+            }
+        } catch (IOException e){
+            Log.e("IOException",e.getMessage());
             e.printStackTrace();
         }
-        Log.v("created testExtFile", testExtFile.getPath());
-        Log.v("testExtFile size", ""+testExtFile.length());
-        String source = testExtFile.getPath();
-        Log.v("trying source", source);
 
         // initializing first song
-        nowPlaying = new Song(source, cache.getPath(), Song.SRC_LOCAL);
+        nowPlaying = playQueue.dequeue();
         try {
             nowPlaying.cache();
         } catch (IOException e) {
@@ -97,9 +89,17 @@ public class HostActivity extends AppCompatActivity {
         }
         Log.v("cached copy", nowPlaying.getCachedCopy().getPath());
 
+        ArrayList<String> titles = new ArrayList<>();
+        for (Song s : playQueue.toArray())
+            titles.add(s.getName() + " - " + s.getArtist());
+
+        queueAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles);
+        lvPlayQueue.setAdapter(queueAdapter);
+
         FloatingActionButton fabConnect = (FloatingActionButton) findViewById(R.id.fabConnect);
         Button btnAddMusic = (Button) findViewById(R.id.btnAddMusic);
         Switch swtPlay = (Switch) findViewById(R.id.swtPlay);
+        Button btnSkip = (Button) findViewById(R.id.btnSkip);
 
         // initializing player
         mp = new MediaPlayer();
@@ -111,8 +111,6 @@ public class HostActivity extends AppCompatActivity {
             Log.e("mediaplayer error", e.getMessage());
             e.printStackTrace();
         }
-
-//        Toast.makeText(getApplicationContext(), "Now playing: Ruby - Warren Malone", Toast.LENGTH_SHORT).show();
 
 
         fabConnect.setOnClickListener(new View.OnClickListener() {
@@ -142,6 +140,14 @@ public class HostActivity extends AppCompatActivity {
             }
         });
 
+        btnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Song skipped", Toast.LENGTH_SHORT).show();
+                nextSong();
+            }
+        });
+
         // makeshift song veto feature
         lvPlayQueue.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
@@ -159,35 +165,82 @@ public class HostActivity extends AppCompatActivity {
         mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
-                playSong();
+                nextSong();
             }
         });
 
 //comment
     }
 
+    private File extFileFromRawAsset(File ext, String assetName, String fileExt) throws IOException {
+        File test = new File(ext+File.separator+assetName+"."+fileExt);
+        Log.v("init "+assetName, test.getPath());
+        Log.v(assetName+" deleted", test.delete() ? "yes" : "no");
+        if (!test.exists()) try {
+            InputStream is = getResources().openRawResource(
+                    getResources().getIdentifier(assetName, "raw", getPackageName()));
+
+            int size = is.available();
+            Log.v("Size mismatch?", size != test.length() ? "yes" : "no");
+            test.createNewFile();
+            Log.v(assetName+" input size", ""+size);
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+
+            FileOutputStream fos = new FileOutputStream(test);
+            fos.write(buffer);
+            fos.close();
+        } catch (IOException e) {
+            Log.e("Write error",e.getMessage());
+            e.printStackTrace();
+        }
+        Log.v("created "+assetName, test.getPath());
+        Log.v(assetName+" size", ""+test.length());
+        return test;
+    }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if (requestCode == REQUEST_CODE && resultCode == RESULT_OK){
             String song = data.getExtras().getString("song");
-            enqueueSong(song);
-            Toast.makeText(getApplicationContext(), song+" added to queue!", Toast.LENGTH_SHORT).show();
+//            enqueueSong(song);
+            Toast.makeText(getApplicationContext(), song+" not really added to queue!", Toast.LENGTH_SHORT).show();
         }
     }
 
     protected void refreshList(){
-        queueAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, playQueue.toArray());
+        ArrayList<String> titles = new ArrayList<>();
+        for (Song s : playQueue.toArray())
+            titles.add(s.getName() + " - " + s.getArtist());
+        queueAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titles);
         lvPlayQueue.setAdapter(queueAdapter);
     }
 
-    protected void enqueueSong(String song){
-        playQueue.enqueue(song);
+    protected void enqueueSong(Song s){
+        playQueue.enqueue(s);
         refreshList();
     }
 
-    protected String dequeueSong(){
-        String song = playQueue.dequeue();
+    protected Song dequeueSong(){
+        Song s = playQueue.dequeue();
         refreshList();
-        return song;
+        return s;
+    }
+
+    protected void nextSong(){
+        nowPlaying = playQueue.dequeue();
+        mp.reset();
+        try {
+            if (!nowPlaying.isCached())
+                nowPlaying.cache();
+            mp.setDataSource(nowPlaying.getCachedCopy().getPath());
+            mp.prepare();
+        } catch (IOException e) {
+            Log.e("mediaplayer error", e.getMessage());
+            e.printStackTrace();
+        }
+        playSong();
+        refreshList();
     }
 
     protected void playSong(){
